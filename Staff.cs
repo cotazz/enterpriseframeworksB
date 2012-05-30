17aa3a0552bf9371b859b5bbaca8496fc6a7fc21
unknown <Administrator@WIN-WGH8JLRNRGW.(none)>
2012-05-30 20:48:43 +0100

    sudent project

---
+++ Staff.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StudentDemo.Yo
+{
+    class Staff
+    {
+        int officeNum;
+
+        public int getOfficeNum()
+        {
+            return this.officeNum;
+        }
+
+        public void setOfficeNum(int officeNum)
+        {
+            this.officeNum = officeNum;
+        }
+    }
+}
