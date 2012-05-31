106b6d38c6aa6617e00786d07f299d229391153b
Brian Burns <bud_weiser3@hotmail.com>
2012-06-01 00:40:32 +0100

    attempting the exercises

--- Program.cs
+++ Program.cs
@@ -27,15 +27,22 @@ namespace StudentDemo.Yo
             // 2. Create an array of Person called people and then create 
             //    some staff and students to add to that array
             //    Write a for loop to iterate through the array and call the toString method
+            Student s = new Student();
+            Person[] people =  { p, s };
+            foreach (Person x in people)
+            {
+                 Console.WriteLine(x.toString());
+            }
             // 3. Create a unit test for our toString class
             // 4. Add a list of modules to a student  (dynamic array)
             // 5. Create a method that lets a course be added to a student
-            
-
+            s.addCourse("Enterprise Frameworks");
 
 
             // 6. For bonus points create a method that can remove a module from a student
 
         }
+
+        
     }
 }
--- Student.cs
+++ Student.cs
@@ -5,10 +5,11 @@ using System.Text;
 
 namespace StudentDemo.Yo
 {
-    class Student
+    class Student : Person
     {
         // Alternative way to specify setter / getter
         private int idNum;
+        private Array modules;
 
         public int IdNum
         {
@@ -20,6 +21,10 @@ namespace StudentDemo.Yo
             {
                 return idNum;
             }
+        }
+        public void addCourse(String course)
+        {
+             this.modules.SetValue(course, this.modules.Length);
         }
     }
 }
