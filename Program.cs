c5ca10ad0a8c8918b39bc6559a653db3be7183e2
Brian Burns <bud_weiser3@hotmail.com>
2012-06-08 19:36:13 +0100

    Compare Universities through ICompare Interface

--- Program.cs
+++ Program.cs
@@ -11,35 +11,23 @@ namespace StudentDemo.Yo
     {
         static void Main(string[] args)
         {
-            Person p = new Person();
-            p.setAge(30);
+            // working with IComparable interface on University objects:
 
-            Console.WriteLine(p.toString());
-             // Brian test
-            // Laks Test 22:11 30/05/2012
-            // Exercises:
-            // sean test
-            // Torsby testing github 
+             University ucd = new University(10000, "UCD");
+             University dcu = new University(5000, "DCU");
+             University nui = new University(5000, "NUI");
+             
+             // This will not work because IComparable is not directly available:
+             // Console.WriteLine("Is nui == to dcu? {0}", nui.CompareTo(dcu)); // Doesn't work
 
+             // The IComparable is hidden untill the university is 'cast' to IComparable:
+             IComparable ucd1 = (IComparable)ucd;
+             Console.WriteLine("Is ucd greater (1), equal (0), less than (-1) dcu? {0}", ucd1.CompareTo(dcu)); // Works
 
-
-            // 1. Add back in our constructor which takes name and age
-            // 2. Create an array of Person called people and then create 
-            //    some staff and students to add to that array
-            //    Write a for loop to iterate through the array and call the toString method
-            Student s = new Student();
-            ArrayList people =  new ArrayList();
-            people.Add(s);
-            people.Add(p);
-            foreach (Person x in people)
-            {
-                 Console.WriteLine(x.toString());
-            }
-            // 3. Create a unit test for our toString class
-            // 4. Add a list of modules to a student  (dynamic array)
-            // 5. Create a method that lets a course be added to a student
-            s.addCourse("Enterprise Frameworks");
-            // 6. For bonus points create a method that can remove a module from a student
+             University[] uni_array = { dcu,ucd , nui };
+             Console.WriteLine("order before sort: {0}, {1}, {2}", uni_array[0], uni_array[1], uni_array[2]);
+             Array.Sort(uni_array); // Sort avails of the IComparable interface defined in University class
+             Console.WriteLine("order after sort: {0}, {1}, {2}", uni_array[0], uni_array[1], uni_array[2]);
             Console.ReadLine(); // pause the output window
         }
 
---
+++ University.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StudentDemo
+{
+     class University : IComparable // IComparable Interface requires implementation of CompareTo method
+     {
+          public int StudentNumber { get; set; }
+          public string Name { get; set; }
+
+          public University(int students, string name)
+          {
+               StudentNumber = students;
+               Name = name;
+          }
+
+          //int IComparable.CompareTo(object obj)
+          //{
+          //     University temp = obj as University;
+          //     if (temp != null)
+          //     {
+          //          if (this.StudentNumber > temp.StudentNumber)
+          //               return 1; // this item comes after the object in sort order
+          //          if (this.StudentNumber < temp.StudentNumber)
+          //               return -1; // this item must come before the object in the sort order
+          //          else
+          //               return 0; // this instances student number is equal to the objects
+          //     }
+          //     else
+          //          throw new ArgumentException("Paramater is not a University!!");
+          //}
+
+          // Can also implement above logic using Integer's built in CompareTo method
+          int IComparable.CompareTo(object obj)
+          {
+               University temp = obj as University;
+               if (temp != null)
+                    return this.StudentNumber.CompareTo(temp.StudentNumber);
+               else
+                    throw new ArgumentException("Paramater is not a University!!");
+          }
+
+
+          public override string ToString()
+          {
+               return this.Name;
+          }
+
+     }
+}
