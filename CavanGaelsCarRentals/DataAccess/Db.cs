8b2c6880de40311d6f48a274c3041b8dc51beddc
seanos1 <seannoshea@hotmail.co.uk>
2012-07-19 15:24:00 +0100

    move db to dal

--- CavanGaelsCarRentals/DataAccess/Db.cs
+++ CavanGaelsCarRentals/DataAccess/Db.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Data.Entity;
 using CavanGaelsCarRentals.Models.ui;
+using CavanGaelsCarRentals.Models;
 
-namespace CavanGaelsCarRentals.Models
+namespace CavanGaelsCarRentals.DataAccess
 {
      public class Db : DbContext
      {
--- CavanGaelsCarRentals/Logic/ServiveLayer.cs
+++ CavanGaelsCarRentals/Logic/ServiveLayer.cs
@@ -5,7 +5,31 @@ using System.Web;
 
 namespace CavanGaelsCarRentals.Logic
 {
-    public class ServiveLayer
+    public class ServiveLayer : IServiceLayer
     {
+        public Models.ui.LocationsUI ListAvailableLocations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Models.ui.LocationCarsCount TotalCarsAvailable(string location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Models.ui.BookingUI ListAvailableCars(Models.ui.LocationsUI requestedTimePlace)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Models.ui.BookingCreateUI ShowChosenCar(Models.ui.BookingUI selectedCar)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Models.ui.BookingConfirmUI ShowBookingConfirm(Models.ui.BookingCreateUI booking)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
